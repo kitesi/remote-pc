eae7dc15dffcf54fdfb910c40784c30750958feb
karizma <karizma7@protonmail.com>
2021-11-25 20:41:13 -0600

    fix audio on linux & change to es2020
    audio info / setting was not working for me on linux
    since amixer needed -D pulse. No way to set the device with
    loudness so I had to implement it myself
    
    implementing it myself meant i needed execa (or i could just use
    child_process but lower level and too lazy) but latest version of execa
    has no require support so had to switch to es modules

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 // @ts-check
-const audioControl = require('loudness');
-const express = require('express');
-const WebSocket = require('ws');
-const http = require('http');
-const path = require('path');
+import WebSocket from 'ws';
+import express from 'express';
+
+import http from 'http';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import { getMuted, getVolume, setMuted, setVolume } from './controller.js';
 
 const app = express();
 const server = http.createServer(app);
@@ -14,8 +17,10 @@ const port = 8080;
 let robot;
 
 try {
-	robot = require('robotjs');
+    robot = require('robotjs');
 } catch (e) {}
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 server.listen(port, () => console.log('ready at localhost:' + port));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -26,70 +31,74 @@ app.use(express.static(path.join(__dirname, 'public')));
  * @param {object} data
  */
 function broadcastData(currentSocket, data) {
-	websocketServer.clients.forEach((client) => {
-		if (client !== currentSocket && client.readyState === WebSocket.OPEN) {
-			client.send(JSON.stringify(data));
-		}
-	});
+    websocketServer.clients.forEach((client) => {
+        if (client !== currentSocket && client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify(data));
+        }
+    });
 }
 
 websocketServer.on('connection', async (socket) => {
-	Promise.all([audioControl.getVolume(), audioControl.getMuted()]).then(
-		([volume, muted]) => {
-			socket.send(
-				JSON.stringify({
-					volume,
-					muted,
-					messageName: 'initial-data',
-					isRobotJSInstalled: !!robot,
-				})
-			);
-		}
-	);
+    Promise.all([getVolume(), getMuted()]).then(([volume, muted]) => {
+        socket.send(
+            JSON.stringify({
+                volume,
+                muted,
+                messageName: 'initial-data',
+                isRobotJSInstalled: !!robot,
+            })
+        );
+    });
 
-	socket.on('message', function (message) {
-		/** @type {import("./src/types.js").PostDataOptions} */
-		const data = JSON.parse(message.toString());
+    socket.on('message', function (message) {
+        /** @type {import("./src/types.js").PostDataOptions} */
+        const data = JSON.parse(message.toString());
 
-		if ('volume' in data) {
-			audioControl.setVolume(data.volume);
-			broadcastData(socket, data);
-		} else if ('muted' in data) {
-			audioControl.setMuted(data.muted);
-			broadcastData(socket, data);
-		} else if (data.key) {
-			const { type, content, modifiers = [] } = data.key;
+        if ('volume' in data) {
+            setVolume(data.volume);
+            broadcastData(socket, data);
+        } else if ('muted' in data) {
+            setMuted(data.muted);
+            broadcastData(socket, data);
+        } else if (data.key) {
+            const { type, content, modifiers = [] } = data.key;
 
-			switch (type) {
-				case 'tap':
-					try {
-						robot.keyTap(content, modifiers);
-					} catch (e) {
-						console.error(e);
-					}
+            switch (type) {
+                case 'tap':
+                    try {
+                        robot.keyTap(content, modifiers);
+                    } catch (e) {
+                        console.error(e);
+                    }
 
-					break;
-				case 'type':
-					robot.typeString(content);
-					break;
-			}
-		} else if (data.mouse) {
-			const type = data.mouse.type;
-			const current = robot.getMousePos();
+                    break;
+                case 'type':
+                    robot.typeString(content);
+                    break;
+            }
+        } else if (data.mouse) {
+            const type = data.mouse.type;
+            const current = robot.getMousePos();
 
-			switch (type) {
-				case 'click':
-					robot.mouseClick(data.mouse.button, data.mouse.double);
-					break;
-				case 'move-mouse':
-					robot.moveMouse(current.x + data.mouse.x, current.y + data.mouse.y);
-					break;
-				case 'drag-mouse':
-					robot.mouseToggle('down');
-					robot.dragMouse(current.x + data.mouse.x, current.y + data.mouse.y);
-					robot.mouseToggle('up');
-					break;
-			}
-		}
-	});
+            switch (type) {
+                case 'click':
+                    robot.mouseClick(data.mouse.button, data.mouse.double);
+                    break;
+                case 'move-mouse':
+                    robot.moveMouse(
+                        current.x + data.mouse.x,
+                        current.y + data.mouse.y
+                    );
+                    break;
+                case 'drag-mouse':
+                    robot.mouseToggle('down');
+                    robot.dragMouse(
+                        current.x + data.mouse.x,
+                        current.y + data.mouse.y
+                    );
+                    robot.mouseToggle('up');
+                    break;
+            }
+        }
+    });
 });
