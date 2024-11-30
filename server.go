458db700f1c8fdb3d3a65f536150b706742f1907
karizma <karizma7@protonmail.com>
2024-11-29 23:37:24 -0500

    print port dynamically rather than hard coded

--- server.go
+++ server.go
@@ -175,7 +175,7 @@ func main() {
 
 	setupRoutes()
 
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
 		log.Fatal(err)
 	}
 }
